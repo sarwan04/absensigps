ed71e8f24a9965cc96928a1d85cda84eb5b9e855
sarwan99 <mulai0.01persen@gmail.com>
2024-08-26 10:33:19 +0700

    Validasi Dupilicate entry Departemen

--- app/Http/Controllers/DepartemenController.php
+++ app/Http/Controllers/DepartemenController.php
@@ -32,6 +32,11 @@ class DepartemenController extends Controller
             'nama_dept' => $nama_dept
         ];
 
+        $cek = DB::table('departemen')->where('kode_dept', $kode_dept)->count();
+        if ($cek > 0) {
+            return Redirect::back()->with(['warning' => 'Data degan Kode Dept ' . $kode_dept . ' Sudah Ada']);
+        }
+
         $simpan = DB::table('departemen')->insert($data);
         if ($simpan) {
             return Redirect::back()->with(['success' => 'Data Berhasil Disimpan']);
