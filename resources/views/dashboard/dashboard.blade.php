63c9624675d127a993b3302cb0ea080fbbfcbdd4
sarwan99 <mulai0.01persen@gmail.com>
2024-08-26 09:01:49 +0700

    Perbaikan Proses logout user

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,7 +1,23 @@
 @extends('layouts.presensi')
 @section('content')
 
+<style>
+    .logout{
+        position: absolute;
+        color: white;
+        font-size: 30px;
+        text-decoration: none;
+        right: 8px;
+    }
+    .logout:hover{
+        color: white;
+    }
+</style>
+
 <div class="section" id="user-section">
+    <a href="/proseslogout" class="logout">
+        <ion-icon name="exit-outline"></ion-icon>
+    </a>
     <div id="user-detail">
         <div class="avatar">
             @if(!empty (Auth::guard('karyawan')->user()->foto))
