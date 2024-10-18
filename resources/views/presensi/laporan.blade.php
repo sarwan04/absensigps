35bef1c3fd7ef7e987ad234a950399114b85baaf
sarwan99 <mulai0.01persen@gmail.com>
2024-10-19 05:50:49 +0700

    Ubah title option value dari karyawan menjadi pegawai, dan disable button export excel

--- resources/views/presensi/laporan.blade.php
+++ resources/views/presensi/laporan.blade.php
@@ -17,7 +17,7 @@
   <div class="page-body">
     <div class="container-xl">
         <div class="row">
-            <div class="col-6">
+            <div class="col-12">
                 <div class="card">
                     <div class="card-body">
                         <form action="/presensi/cetaklaporan" id="frmLaporan" target="_blank" method="POST">
@@ -57,7 +57,7 @@
                                 <div class="col-12">
                                     <div class="form-group">
                                         <select name="nik" id="nik" class="form-select">
-                                            <option value="">Karyawan</option>
+                                            <option value="">Pegawai</option>
                                             @foreach ($karyawan as $d)
                                                 <option value="{{ $d->nik}}">{{ $d->nama_lengkap}}</option>
                                             @endforeach
@@ -67,7 +67,7 @@
                             </div> 
 
                             <div class="row mt-2">
-                                <div class="col-6">
+                                <div class="col-12">
                                     <div class="form-group">
                                        <button type="submit" name="cetak" class="btn btn-primary w-100">
                                         <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-printer"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M17 17h2a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h2" /><path d="M17 9v-4a2 2 0 0 0 -2 -2h-6a2 2 0 0 0 -2 2v4" /><path d="M7 13m0 2a2 2 0 0 1 2 -2h6a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-6a2 2 0 0 1 -2 -2z" /></svg>
@@ -76,14 +76,14 @@
                                     </div>
                                 </div>
 
-                                <div class="col-6">
+                                {{-- <div class="col-6">
                                     <div class="form-group">
                                        <button type="submit" name="exportexcel" class="btn btn-success w-100">
                                         <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-download"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" /><path d="M7 11l5 5l5 -5" /><path d="M12 4l0 12" /></svg>
                                         Export to Excel
                                        </button>
                                     </div>
-                                </div>
+                                </div> --}}
                             </div>
 
                         </form>
