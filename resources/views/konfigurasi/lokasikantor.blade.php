4df04ee1afd55e52e319c412ed9c0aafe02af884
sarwan99 <mulai0.01persen@gmail.com>
2024-11-11 06:39:12 +0700

    Setting maxlengt di form input konfigurasi lokasi kantor

--- resources/views/konfigurasi/lokasikantor.blade.php
+++ resources/views/konfigurasi/lokasikantor.blade.php
@@ -39,7 +39,7 @@
                                         <span class="input-icon-addon">
                                           <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-map-pin"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /><path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" /></svg>
                                         </span>
-                                        <input type="text" value="{{ $lok_kantor->lokasi_kantor }}" id="lokasi_kantor" class="form-control" name="lokasi_kantor" placeholder="Lokasi Kantor">
+                                        <input type="text" value="{{ $lok_kantor->lokasi_kantor }}" id="lokasi_kantor" class="form-control" name="lokasi_kantor" placeholder="Lokasi Kantor" maxlength="250">
                                       </div>
                                 </div>
                             </div>
@@ -50,7 +50,7 @@
                                         <span class="input-icon-addon">
                                           <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-radar-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M15.51 15.56a5 5 0 1 0 -3.51 1.44" /><path d="M18.832 17.86a9 9 0 1 0 -6.832 3.14" /><path d="M12 12v9" /></svg>
                                         </span>
-                                        <input type="text" value="{{ $lok_kantor->radius }}" id="radius" class="form-control" name="radius" placeholder="Radius">
+                                        <input type="number" value="{{ $lok_kantor->radius }}" id="radius" class="form-control" name="radius" placeholder="Radius" maxlength="5">
                                       </div>
                                 </div>
                             </div>
