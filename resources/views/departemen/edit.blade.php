72ff9ae1bd1060c8e05b81c629bb9ccff33d7686
sarwan99 <mulai0.01persen@gmail.com>
2024-11-14 04:19:02 +0700

    Perbaikan dan menambahkan validasi pada form input dan edit data jabatan

--- resources/views/departemen/edit.blade.php
+++ resources/views/departemen/edit.blade.php
@@ -25,16 +25,28 @@
         <div class="col-12">
             <div class="input-icon mb-3">
                 <span class="input-icon-addon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-user">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-user">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
                         <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
                     </svg>
                 </span>
-                <input type="text" value="{{ $departemen->nama_dept }}" id="nama_dept" class="form-control" name="nama_dept" placeholder="Nama Jabatan" maxlength="50">
+                <input 
+                    type="text" 
+                    value="{{ $departemen->nama_dept }}" 
+                    id="nama_dept" 
+                    class="form-control" 
+                    name="nama_dept" 
+                    placeholder="Nama Jabatan" 
+                    maxlength="50" 
+                    required 
+                    oninvalid="this.setCustomValidity('Nama jabatan harus diisi dan tidak boleh kosong!')" 
+                    oninput="this.setCustomValidity('')"
+                >
             </div>
         </div>
     </div>
+    
 
     <div class="row mt-2">
         <div class="col-12">
--- resources/views/departemen/index.blade.php
+++ resources/views/departemen/index.blade.php
@@ -124,7 +124,7 @@
                               <!-- Download SVG icon from http://tabler-icons.io/i/user -->
                               <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-barcode"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7v-1a2 2 0 0 1 2 -2h2" /><path d="M4 17v1a2 2 0 0 0 2 2h2" /><path d="M16 4h2a2 2 0 0 1 2 2v1" /><path d="M16 20h2a2 2 0 0 0 2 -2v-1" /><path d="M5 11h1v2h-1z" /><path d="M10 11l0 2" /><path d="M14 11h1v2h-1z" /><path d="M19 11l0 2" /></svg>
                             </span>
-                            <input type="text" value="" id="kode_dept" class="form-control" placeholder="Kode Jabatan" name="kode_dept" maxlength="5" >
+                            <input type="text" value="" id="kode_dept" class="form-control" placeholder="Kode Jabatan" name="kode_dept" maxlength="5" required oninvalid="this.setCustomValidity('Kode jabatan tidak boleh kosong!')" oninput="setCustomValidity('')">
                           </div>
                     </div>
                 </div>
@@ -136,7 +136,7 @@
                               <!-- Download SVG icon from http://tabler-icons.io/i/user -->
                               <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-user"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
                             </span>
-                            <input type="text" value="" id="nama_dept" class="form-control" name="nama_dept" placeholder="Nama Jabatan" maxlength="50">
+                            <input type="text" value="" id="nama_dept" class="form-control" name="nama_dept" placeholder="Nama Jabatan" maxlength="50" required oninvalid="this.setCustomValidity('Nama jabatan tidak boleh kosong!')" oninput="setCustomValidity('')">                     
                           </div>
                     </div>
                 </div>
@@ -255,35 +255,6 @@
                     form.submit(); 
                 }
             });
-        });
-        
-          // Validasi form tambah Jabatan
-          $("#frmDepartemen").submit(function() {
-            var kode_dept = $("#kode_dept").val();
-            var nama_dept = $("#nama_dept").val();
-    
-            // Validasi setiap field
-            if (kode_dept == "") {
-                Swal.fire({
-                    title: 'Warning!',
-                    text: 'Kode Jabatan Harus di Isi',
-                    icon: 'warning',
-                    confirmButtonText: 'OK'
-                }).then(() => {
-                    $("#kode_dept").focus();
-                });
-                return false;
-            } else if (nama_dept == "") {
-                Swal.fire({
-                    title: 'Warning!',
-                    text: 'Nama Jabatan Harus di Isi',
-                    icon: 'warning',
-                    confirmButtonText: 'OK'
-                }).then(() => {
-                    $("#nama_dept").focus();
-                });
-                return false;
-            } 
         });
 
     });
