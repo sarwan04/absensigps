2293d7dec13c97bec4d9dd742a5812adb12d4c85
sarwan99 <mulai0.01persen@gmail.com>
2024-11-14 05:04:34 +0700

    Merapikan kode pada konfigurasi lokasi kantor

--- resources/views/konfigurasi/lokasikantor.blade.php
+++ resources/views/konfigurasi/lokasikantor.blade.php
@@ -1,94 +1,120 @@
 @extends('layouts.admin.tabler')
 
 @section('content')
-<div class="page-header d-print-none">
-    <div class="container-xl">
-      <div class="row g-2 align-items-center">
-        <div class="col">
-          <h2 class="page-title">
-            Konfigurasi Lokasi
-          </h2>
+    <div class="page-header d-print-none">
+        <div class="container-xl">
+            <div class="row g-2 align-items-center">
+                <div class="col">
+                    <h2 class="page-title">
+                        Konfigurasi Lokasi
+                    </h2>
+                </div>
+
+            </div>
         </div>
-    
-      </div>
     </div>
-  </div>
 
-  <div class="page-body">
-    <div class="container-xl">
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-body">
-                        @if (Session::get('success'))
-                        <div class="alert alert-success">
-                                {{ Session::get('success')}}
+    <div class="page-body">
+        <div class="container-xl">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            @if (Session::get('success'))
+                                <div class="alert alert-success">
+                                    {{ Session::get('success') }}
+                                </div>
+                            @endif
+
+                            @if (Session::get('warning'))
+                                <div class="alert alert-warning">
+                                    {{ Session::get('warning') }}
+                                </div>
+                            @endif
+                            <form action="/konfigurasi/updatelokasikantor" method="POST">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="input-icon mb-3">
+                                            <span class="input-icon-addon">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                    class="icon icon-tabler icons-tabler-outline icon-tabler-map-pin">
+                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                    <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                                                    <path
+                                                        d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+                                                </svg>
+                                            </span>
+                                            <input type="text" value="{{ $lok_kantor->lokasi_kantor }}"
+                                                id="lokasi_kantor" class="form-control" name="lokasi_kantor"
+                                                placeholder="Lokasi Kantor" maxlength="250">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="input-icon mb-3">
+                                            <span class="input-icon-addon">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                    class="icon icon-tabler icons-tabler-outline icon-tabler-radar-2">
+                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                    <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+                                                    <path d="M15.51 15.56a5 5 0 1 0 -3.51 1.44" />
+                                                    <path d="M18.832 17.86a9 9 0 1 0 -6.832 3.14" />
+                                                    <path d="M12 12v9" />
+                                                </svg>
+                                            </span>
+                                            <input type="number" value="{{ $lok_kantor->radius }}" id="radius"
+                                                class="form-control" name="radius" placeholder="Radius" maxlength="5">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-12">
+                                        <button class="btn btn-primary w-100">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                                stroke-linecap="round" stroke-linejoin="round"
+                                                class="icon icon-tabler icons-tabler-outline icon-tabler-refresh">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" />
+                                                <path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" />
+                                            </svg>
+                                            Update
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                        @endif
-
-                        @if (Session::get('warning'))
-                            <div class="alert alert-warning">
-                                {{ Session::get('warning')}}
-                            </div>
-                        @endif
-                        <form action="/konfigurasi/updatelokasikantor" method="POST">
-                            @csrf
-                            <div class="row">
-                                <div class="col-12">
-                                    <div class="input-icon mb-3">
-                                        <span class="input-icon-addon">
-                                          <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-map-pin"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /><path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" /></svg>
-                                        </span>
-                                        <input type="text" value="{{ $lok_kantor->lokasi_kantor }}" id="lokasi_kantor" class="form-control" name="lokasi_kantor" placeholder="Lokasi Kantor" maxlength="250">
-                                      </div>
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-12">
-                                    <div class="input-icon mb-3">
-                                        <span class="input-icon-addon">
-                                          <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-radar-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M15.51 15.56a5 5 0 1 0 -3.51 1.44" /><path d="M18.832 17.86a9 9 0 1 0 -6.832 3.14" /><path d="M12 12v9" /></svg>
-                                        </span>
-                                        <input type="number" value="{{ $lok_kantor->radius }}" id="radius" class="form-control" name="radius" placeholder="Radius" maxlength="5">
-                                      </div>
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-12">
-                                    <button class="btn btn-primary w-100">
-                                        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-refresh"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" /><path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" /></svg>
-                                        Update
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-  </div>
+@endsection
 
-  @endsection
+@push('myscript')
+    <script>
+        $(function() {
 
-  @push('myscript')
-   <script>
-     $(function(){
+            $(document).ready(function() {
+                // Menghilangkan pesan sukses setelah 3 detik
+                setTimeout(function() {
+                    $('.alert-success').fadeOut('slow');
+                }, 3000);
 
-        $(document).ready(function() {
-            // Menghilangkan pesan sukses setelah 3 detik
-            setTimeout(function() {
-                $('.alert-success').fadeOut('slow');
-            }, 3000);
+                // Menghilangkan pesan gagal setelah 3 detik
+                setTimeout(function() {
+                    $('.alert-warning').fadeOut('slow');
+                }, 3000);
+            });
 
-            // Menghilangkan pesan gagal setelah 3 detik
-            setTimeout(function() {
-                $('.alert-warning').fadeOut('slow');
-            }, 3000);
         });
-
-    });
-   </script>
+    </script>
 @endpush
